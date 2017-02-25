2d22eaca588f88dee1a182c7819457c8808d807d
MengRao <278307367@qq.com>
2017-02-25 12:42:27 +0800

    speed up modint,while care should be taken in using it

--- Math/ModInt.h
+++ Math/ModInt.h
@@ -3,8 +3,8 @@ struct ModInt {
   static const int Mod = MOD;
   unsigned x;
   ModInt() : x(0) { }
-  ModInt(signed sig) : x(sig % MOD) {  }
-  ModInt(signed long long sig) : x(sig % MOD)  { }
+  ModInt(signed sig) : x(sig) {  }
+  ModInt(signed long long sig) : x(sig)  { }
   int get() const { return (int)x; }
   ModInt power(unsigned p) { ModInt res = 1, a = *this; while (p) { if (p & 1) res *= a; a *= a; p >>= 1; } return res; }
 
