f2277aa7fd4377d11365052e16158348fdc01613
MengRao <278307367@qq.com>
2017-07-11 20:00:38 +0800

    update Matrix

--- Math/Matrix.h
+++ Math/Matrix.h
@@ -1,51 +1,46 @@
-struct mat
-{
-	int m, n;
-	vector< vector<long long> > arr;
+template<typename T>
+struct Matrix {
+  vector<vector<T>> a;
 
-	mat()
-		: m(0)
-		, n(0)
-	{
-	}
+  vector<T>& operator[](int x) { return a[x]; }
 
-	void init(int m_, int n_)
-	{
-		m = m_;
-		n = n_;
-		arr.resize(m);
-		for (auto &r:arr)
-		{
-			r.resize(n, 0);
-		}
-	}
-	
+  const vector<T>& operator[](int x) const { return a[x]; }
+
+  int row() const { return a.size(); }
+  int col() const { return a[0].size(); }
+
+  Matrix(int r, int c) {
+    a.resize(r);
+    for (auto& e : a) e.resize(c, 0);
+  }
+
+  Matrix trans() const {
+    Matrix ret(col(), row());
+    rep(i, row()) rep(j, col()) ret[j][i] = a[i][j];
+    return ret;
+  }
+
+  Matrix operator*(const Matrix& rhs) { // col() == rhs.row()
+    Matrix ret(row(), rhs.col());
+    //rep(i,row()) rep(j,rhs.col()) rep(k,col()) ret[i][j] += a[i][k] * rhs[k][j];
+    Matrix b = rhs.trans();
+    rep(i, row()) rep(j, b.row()) rep(k, col()) ret[i][j] += a[i][k] * b[j][k];
+    return ret;
+  }
+
+  Matrix &operator*=(const Matrix& rhs) { return *this = *this * rhs; }
+
+  Matrix pow(long long p) { // row() == col()
+    Matrix ret(row(), row());
+    rep(i, row()) ret[i][i] = 1;
+    Matrix t = *this;
+    while (p) {
+      if (p & 1) ret *= t;
+      t *= t;
+      p >>= 1;
+    }
+    return ret;
+  }
+
 };
-
-mat multiplyMat(const mat& a, const mat& b, int m)
-{
-	mat sol;
-	sol.init(a.m, b.n);
-	for (int i = 0; i < a.m; i++)
-		for (int j = 0; j < b.n; j++)
-			for (int k = 0; k < a.n; k++)
-				sol.arr[i][j] = (sol.arr[i][j] + a.arr[i][k] * b.arr[k][j]) % m;
-	return sol;
-}
-
-// mat a should be square
-mat matpower(mat a, int p, int m)
-{
-	mat ret;
-	ret.init(a.m, a.m);
-	rep(i, a.m)
-		ret.arr[i][i] = 1;
-	while(p)
-	{
-		if (p%2)
-			ret = multiplyMat(ret, a, m);
-		a = multiplyMat(a, a, m);
-		p /= 2;
-	}
-	return ret;
-}
+typedef Matrix<int> Mat;
