4285c416de1f9511caae65668141955e51c51a97
MengRao <278307367@qq.com>
2017-07-14 16:11:39 +0800

    restructure gcd_ext

--- Math/GCD.h
+++ Math/GCD.h
@@ -1,1 +1,14 @@
 template<class T> T gcd(T a, T b) { return b ? gcd(b, a % b) : a; }
+
+//ax + by = gcd(a, b)
+template<class T>
+T gcd_ext(T a, T b, T& x, T& y) {
+  if (b == 0) {
+    x = 1;
+    y = 0;
+    return a;
+  }
+  T g = gcd_ext(b, a % b, y, x);
+  y -= (a / b) * x;
+  return g;
+}
