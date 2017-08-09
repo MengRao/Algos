71395502651ecc0f52c0fea835b147e43173fd1c
MengRao <278307367@qq.com>
2017-08-09 10:25:49 +0800

    Create prefix function.h

---
+++ Strings/prefix function.h
@@ -0,0 +1,13 @@
+vector<int> prefixFunction(const string& s) {
+    vector<int> p(s.size());
+    int j = 0;
+    for (int i = 1; i < (int)s.size(); i++) {
+        while (j > 0 && s[j] != s[i])
+            j = p[j-1];
+
+        if (s[j] == s[i])
+            j++;
+        p[i] = j;
+    }   
+    return p;
+}
