67726a25b08c5755c57259fbfa3f0e8d436c3e8c
MengRao <278307367@qq.com>
2017-08-23 18:35:16 +0800

    Update BigInteger.h

--- Math/BigInteger.h
+++ Math/BigInteger.h
@@ -22,7 +22,6 @@ struct BigInteger
     }
     int i = str.size()-1;
     if(i==end&&str[i]=='0') return;
-
     while(i>=end){
       int v = 0;
       int pos = 1;
@@ -39,16 +38,13 @@ struct BigInteger
 		if (n1.size() != n2.size()) {
       return n1.size() - n2.size();
 		}
-		for (auto i = n1.size(); i-->0; ) {
-			if (n1[i] != n2[i]) {
-        return n1[i] - n2[i];
-			}
+		for (auto i = n1.size(); i-- > 0; ) {
+			if (n1[i] != n2[i]) return n1[i] - n2[i];
 		}
 		return 0;
 	}
   int compare(const BigInteger &other) const {
-    if(sign != other.sign)
-      return sign - other.sign;
+    if(sign != other.sign) return sign - other.sign;
     return absoluteCmp(num, other.num) * sign;
   }
 	bool operator < (const BigInteger &other) const {
@@ -84,9 +80,7 @@ struct BigInteger
 			}
 			dest.num.push_back(cur);
 		}
-		if (carry) {
-			dest.num.push_back(carry);
-		}
+		if (carry) dest.num.push_back(carry);
 	}
 	static void subtract(BigInteger &dest, const vector<uint32_t> &n1, const vector<uint32_t> &n2) {
 		const vector<uint32_t> *np1 = &n1, *np2 = &n2;
@@ -110,9 +104,7 @@ struct BigInteger
 			}
 			dest.num.push_back(cur);
 		}
-		while (!dest.num.empty() && dest.num.back() == 0) {
-			dest.num.pop_back();
-		}
+		while (dest.num.size() && dest.num.back() == 0) dest.num.pop_back();
 	}
 
 	static void multiply(BigInteger &dest, const vector<uint32_t> &n1, const vector<uint32_t> &n2) {
@@ -126,38 +118,29 @@ struct BigInteger
 			}
 			dest.num[i+n2.size()] += carry;
 		}
-    while (!dest.num.empty() && dest.num.back() == 0)
-      dest.num.pop_back();
+    while (dest.num.size() && dest.num.back() == 0) dest.num.pop_back();
 	}
 
 	static void division(BigInteger &dest, const vector<uint32_t> &n1, const vector<uint32_t> &n2) {
 		dest.num.clear();
 		BigInteger remain(0);
 		remain.num = n1;
-
 		for (int k = n1.size()-n2.size(); k >= 0; k--)
 		{
 			BigInteger sub;
 			sub.num.assign(k, 0);
 			sub.num.insert(sub.num.end(), n2.begin(), n2.end());
 			if (sub > remain) {
-				if (!dest.num.empty()) {
-					dest.num.push_back(0);
-				}
+				if (dest.num.size()) dest.num.push_back(0);
 				continue;
 			}
-
 			int low = 0, high = base - 1;
 			while (low <= high) {
 				int mid = low + high >> 1;
 				BigInteger submul = sub * mid;
-				if (submul <= remain) {
-					low = mid + 1;
-				} else {
-					high = mid - 1;
-				}
+				if (submul <= remain) low = mid + 1;
+				else high = mid - 1;
 			}
-
 			remain -= sub * high;
 			dest.num.push_back(high);
 		}
@@ -173,8 +156,7 @@ struct BigInteger
 			add(ret, num, other.num);
 			ret.sign = sign;
 		}
-		if (ret.num.empty())
-			ret.sign = 1;
+		if (ret.num.empty()) ret.sign = 1;
 		return ret;
 	}
 	BigInteger operator - (const BigInteger &other) const {
@@ -186,47 +168,22 @@ struct BigInteger
 			add(ret, num, other.num);
 			ret.sign = sign;
 		}
-		if (ret.num.empty())
-			ret.sign = 1;
+		if (ret.num.empty()) ret.sign = 1;
 		return ret;
 	}
-	string to_string() const {
-		string res;
-		res.reserve(num.size() * baselen + 2);
-		if (sign == -1) res.push_back('-');
-		int fzero = 1;
-		for (auto i = num.size(); i-->0; ) {
-			uint32_t v = num[i];
-			int pos = base / 10;
-			while (pos) {
-				int digit = v / pos;
-				v %= pos;
-				pos /= 10;
-				if (digit)
-					fzero = 0;
-				if (fzero == 0) {
-					res.push_back("0123456789"[digit]);
-				}
-			}
-		}
-		if (fzero)
-			res.push_back('0');
-		return res;
-	}
+	
 	BigInteger operator * (const BigInteger &other) const {
 		BigInteger ret;
 		ret.sign = sign * other.sign;
 		multiply(ret, num, other.num);
-		if (ret.num.empty())
-			ret.sign = 1;
+		if (ret.num.empty()) ret.sign = 1;
 		return ret;
 	}
 	BigInteger operator / (const BigInteger &other) const {
 		BigInteger ret;
 		ret.sign = sign * other.sign;
 		division(ret, num, other.num);
-		if (ret.num.empty())
-			ret.sign = 1;
+		if (ret.num.empty()) ret.sign = 1;
 		return ret;
 	}
 	BigInteger operator % (const BigInteger &other) const {
@@ -254,25 +211,39 @@ struct BigInteger
 		*this = *this % other;
 		return *this;
 	}
-  BigInteger sqrt(){
-    BigInteger l = 0, r = *this;
-    while(l<=r){
-      BigInteger m = (l+r)/2;
-      if(m*m<=*this) l = m+1;
-      else r = m-1;
-    }
-    return r;
-  }
-	long long get() const 
-	{
-		long long res = 0;
-		long long b = sign;
+  string to_string() const {
+		string res;
+		res.reserve(num.size() * baselen + 2);
+		if (sign == -1) res.push_back('-');
+		int fzero = 1;
+		for (auto i = num.size(); i-->0; ) {
+			uint32_t v = num[i];
+			int pos = base / 10;
+			while (pos) {
+				int digit = v / pos;
+				v %= pos;
+				pos /= 10;
+				if (digit) fzero = 0;
+				if (fzero == 0) res.push_back("0123456789"[digit]);
+			}
+		}
+		if (fzero) res.push_back('0');
+		return res;
+	}
+	long long to_int() const {
+		long long res = 0, b = sign;
 		for (int i = 0; i < num.size(); i++) {
 			res += b * num[i];
 			b *= base;
 		}
 		return res;
 	}
+  friend ostream& operator<<(ostream& o, const BigInteger& b) {
+    return o << b.to_string();
+  }
+  friend istream& operator>>(istream& i, BigInteger& b) {
+    string s; i >> s; b = BigInteger(s); return i;
+  }
 	
 };
 typedef BigInteger bint;
