cac869232797519cb1dc1d8bfd6afa3ca96734d7
MengRao <278307367@qq.com>
2017-09-11 20:54:55 +0800

    minor change

--- Math/ModInt.h
+++ Math/ModInt.h
@@ -3,7 +3,7 @@ struct ModInt {
   unsigned x;
   ModInt() : x(0) { }
   ModInt(signed sig) : x(sig) {  }
-  ModInt(signed long long sig) : x(sig%MOD)  { }
+  ModInt(signed long long sig) : x(sig%MOD) { }
   int get() const { return (int)x; }
   ModInt pow(unsigned p) { ModInt res = 1, a = *this; while (p) { if (p & 1) res *= a; a *= a; p >>= 1; } return res; }
 
@@ -16,5 +16,6 @@ struct ModInt {
   ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
   ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
   ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+  bool operator<(ModInt that) const { return x < that.x; }
 };
 typedef ModInt<1000000007> mint;
