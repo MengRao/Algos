2301b743a26cb881ecb247e87b6ff262de845d2e
MengRao <278307367@qq.com>
2017-12-13 16:39:44 +0800

    Update square root decomposition.h

--- Search/square root decomposition.h
+++ Search/square root decomposition.h
@@ -1,77 +1,87 @@
-
-
-template<int SZ>
 struct SQRT {
-  static constexpr int sqrt(int n, int i = 1) {
-    return n == i ? n : (i * i < n ? sqrt(n, i + 1) : i);
-  }
-  static constexpr int B = sqrt(SZ);
-  static constexpr int NB = (SZ + B - 1) / B;
   typedef int T;
-  struct BLK {
-    array<T, B> a;
+  typedef int RES;
+  static constexpr RES defaultRES = 0; // make sure combine(defaultRES, x) == x
+  static inline RES combine(RES a, RES b){
+    return a + b;
+  }
 
-    BLK() {
-      rep(i, B) a[i] = 0;
+  struct BLK{
+    int n;
+    T* a;
+
+    void init(int n_, T* a_){ // whether values in a_ are set depends on how SQRT.init() was called
+      n = n_;
+      a = a_;
+    }
+
+    // called before partial update and query
+    void push(){
+      // rep(i, n){
+      //   auto& x = a[i];
+      // }
+    }
+
+    // called after paritial update
+    void pull(){
+      // rep(i, n){
+      //   auto& x = a[i];
+      // }
+    }
+
+    void update(T& x, const T& ad){
+
+    }
+
+    void update(const T& ad){
+      
+    }
+
+    RES query(T& x){
+
+    }
+
+    RES query(){
+
     }
   };
-  vector<BLK> blk;
-
-  SQRT():blk(NB) {}
-  
-
-  void push(BLK& b) {
-    fore(x, b.a) {
-
-    }
-  }
-
-  void pull(BLK& b) {
-    fore(x, b.a) {
-
-    }
-  }
-
-  void update(T& x, const T& ad) {
-
-  }
-
-  void update(BLK& b, const T& ad) {
-    
-  }
-
-  typedef ll RES;
-  static constexpr RES defaultRES = 0;
-  RES combine(RES a, RES b) {
-
-  }
-
-  RES query(T& x) {
-
-  }
-
-  RES query(BLK& b) {
-
-  }
 
   //above should be customized
+  int B;
+  T* a;
+  unique_ptr<T[]> _a;
+  unique_ptr<BLK[]> blk;
+  
+  void init(int n, T* a_ = 0){
+    B = sqrt(n);
+    int NB = (n + B - 1) / B;
+    if(a_) a = a_;
+    else{
+      _a.reset(new T[n]);
+      a = _a.get();
+    }
+    blk.reset(new BLK[NB]);
+    rep(i,NB)blk[i].init(min(B,n-i*B+1), &a[i*B]);
+  }
+
   void update(int l, int r, const T& ad) {
     int bl = l / B, br = r / B;
     l %= B, r %= B;
+    auto& b1 = blk[bl];
     if (bl == br) {
-      auto& b = blk[bl];
-      push(b);
-      FOR(i, l, r)update(b.a[i], ad);
-      pull(b);
+      b1.push();
+      FOR(i, l, r) b1.update(b1.a[i], ad);
+      b1.pull();
     }
     else {
-      push(blk[bl]);
-      push(blk[br]);
-      FOR(i, l, B - 1) update(blk[bl].a[i], ad);
-      FOR(i, 0, r) update(blk[br].a[i], ad);
-      pull(blk[bl]);
-      pull(blk[br]);
-      FOR(b, bl + 1, br - 1)update(blk[b], ad);
+      b1.push();
+      FOR(i, l, B - 1) b1.update(b1.a[i], ad);
+      b1.pull();
+      FOR(b, bl + 1, br - 1) blk[b].update(ad);
+      auto& b2 = blk[br];
+      b2.push();
+      FOR(i, 0, r) b2.update(b2.a[i], ad);
+      b2.pull();
     }
   }
 
@@ -79,17 +89,18 @@ struct SQRT {
     int bl = l / B, br = r / B;
     l %= B, r %= B;
     RES ans = defaultRES;
+    auto& b1 = blk[bl];
     if (bl == br) {
-      auto& b = blk[bl];
-      push(b);
-      FOR(i, l, r)ans = combine(ans, query(b.a[i]));
+      b1.push();
+      FOR(i, l, r) ans = combine(ans, b1.query(b1.a[i]));
     }
     else {
-      push(blk[bl]);
-      push(blk[br]);
-      FOR(i, l, B - 1) ans = combine(ans, query(blk[bl].a[i]));
-      FOR(i, 0, r) ans = combine(ans, query(blk[br].a[i]));
-      FOR(b, bl + 1, br - 1) ans = combine(ans, query(blk[b]));
+      b1.push();
+      FOR(i, l, B - 1) ans = combine(ans, b1.query(b1.a[i]));
+      FOR(b, bl + 1, br - 1) ans = combine(ans, blk[b].query());
+      auto& b2 = blk[br];
+      b2.push();
+      FOR(i, 0, r) ans = combine(ans, b2.query(b2.a[i]));
     }
     return ans;
   }
